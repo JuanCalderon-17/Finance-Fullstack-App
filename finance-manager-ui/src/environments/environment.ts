69b363f4ae9bb56c0a477ba1820bb69941773a4f
Juan <juancalderonmicro@hotmail.com>
2025-12-22 21:26:49 -0500

    fixed environment.ts and its slash bug

--- finance-manager-ui/src/environments/environment.ts
+++ finance-manager-ui/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const  environment = {
     production: true, 
-    apiUrl: 'https://finance-fullstack-app.onrender.com/api' //para despliege URL
+    apiUrl: 'https://finance-fullstack-app.onrender.com/api/' //para despliege URL
 } 
