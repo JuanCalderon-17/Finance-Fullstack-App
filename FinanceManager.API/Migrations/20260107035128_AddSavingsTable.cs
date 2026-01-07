76a7f72d925a84d15f31a0d8ca9fadcb2e393ca6
Juan <juancalderonmicro@hotmail.com>
2026-01-06 22:51:32 -0500

    Agregando migracion de tabla de ahorros

---
+++ FinanceManager.API/Migrations/20260107035128_AddSavingsTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceManager.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSavingsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Goal",
+                table: "SavingsAccounts",
+                type: "numeric(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Goal",
+                table: "SavingsAccounts",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,2)",
+                oldNullable: true);
+        }
+    }
+}
